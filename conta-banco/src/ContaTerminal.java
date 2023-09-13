54b55df1cfd958b584e6fb1760fc4f96a018e029
alexandrerosa-dev <asr.alexandrerosa@gmail.com>
2023-09-13 06:13:32 -0300

    projeto conta banco DIO

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,15 @@
+
+public class ContaTerminal {
+
+	public static void main(String[] args) {
+		//TODO: Conhecer e importar a classe Scanner
+		
+		//Exibir as mensagens para o nosso usuário
+		
+		//Obter pela scanner os valores digitados no terminal
+		
+		//Exibir a mensagem conta criada
+
+	}
+
+}
